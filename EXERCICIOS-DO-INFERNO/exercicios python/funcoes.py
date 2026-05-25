c73f098580f88e78f32a8a7a7e3657fc1f7ac7ec
luigi toledo <luigi8anos2015@gmail.com>
2026-05-25 15:05:07 -0300

    Delete EXERCICIOS-DO-INFERNO directory

--- EXERCICIOS-DO-INFERNO/exercicios python/funcoes.py
+++
@@ -1,7 +0,0 @@
-def calculadora_gorjeta(valor, porcentagem):
-    gorjeta = valor * (porcentagem / 100)
-    return gorjeta
-def calcular_total_com_gorjeta(valor, porcentagem):
-    gorjeta = calculadora_gorjeta(valor, porcentagem)
-    total = valor + gorjeta
-    return total
--- EXERCICIOS-DO-INFERNO/exercicios python/main.py
+++
@@ -1,9 +0,0 @@
-from funcoes import calculadora_gorjeta, calcular_total_com_gorjeta
-
-valor = float(input("Digite o valor da conta: "))
-porcentagem = float(input("Digite a porcentagem de gorjeta: "))
-
-gorjeta = calculadora_gorjeta(valor, porcentagem)
-total = calcular_total_com_gorjeta(valor, porcentagem)
-print(f"A gorjeta é de: {gorjeta:.2f}")
-print(f"O total a pagar é de: {total:.2f}")
